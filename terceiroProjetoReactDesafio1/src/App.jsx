5cb2613052ead94f8ef2546976e0c580ecfa5b76
Matheus de Carvalho <matheusdecarvalho100@gmail.com>
2025-02-13 07:33:33 -0300

    Dia_13_02_2025

--- terceiroProjetoReactDesafio1/src/App.jsx
+++ terceiroProjetoReactDesafio1/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
 import Header from "./components/Header"
+import Footer from "./components/FooterProps"
+import FooterProps from './components/FooterProps'
 
 function App() {
   return (
     <>
      <Header/>
+
+
+
+     <FooterProps
+        email="careca@gmail.com"
+        telefone="(19)1942-1845"
+        instagram="https://www.instagram.com"
+        facebook="https://www.facebook.com"
+        twitter="https://www.twitter.com"
+        autor="Grupo 5"
+      
+      />
     </>
   )
 }
