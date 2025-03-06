7dfabc3d26f5665cf2ee17b9f235c7b72d3e2dc0
Laura Marques <lauramarques00000@gmail.com>
2025-03-06 09:33:26 -0300

    Laura_06/03

--- terceiroProjetoReactDesafio1/src/pages/Laura.jsx
+++ terceiroProjetoReactDesafio1/src/pages/Laura.jsx
@@ -1,5 +1,38 @@
-function FaleConosco() {
-    return <h1>📞Fale Conosco</h1>;
-  }
+import NavBar from '../components/NavBar'
+import Header from '../components/Header'
+import './Laura.css'
 
-  export default FaleConosco
+function SobreLaura() {
+  return (
+    <>
+      <Header />
+      
+      <div className='container'>
+
+        <NavBar />
+
+        <div className='aolado'>
+          <h1> Sobre Laura</h1>
+
+          <p>Laura é uma menina muito querida, amorosa e gentil, nasceu em 27 de abril em vinhedo e tem dois irmãos sendo uma menina e um menino.
+            Sempre foi muito amada e cuidada por sua mãe Nathalia Marques e seu pai Diogo Andreotti de Salles Pupo.
+            Estudou no Dr. Abrahão Aun durante e sua infância e esta terminando o ensino médio no SESI vinhedo juntamente ao curso de TI que faz no SENAI.
+            Suas amigas Ana, Laura, Milena e Maria Clara a amam muito e sempre estão juntas.
+            Suas cores favoritas são rosa e verdes, junto ao seu time Palmeiras e Classe-A.
+            Seu gosto musical é muito bom composto por cantores como Drake, Kanye West, Travis Scoot.
+            Ela joga e acompanha futebol e assite muito feliz seu pai jogar também em um time de vinhedo que é da familia da mesma e existe a 15 anos.
+            Ela sonha em ir para Itália (Roma), Espanha (Madrid), Portugal (Lisboa), França (Paris) explorando diversas culturas.
+            Pretende estudar para ser Fisioterapeuta Esportiva, então atletas, aguardem!!!
+            Suas matérias favoritas são biologia e educação fisica.
+            Ela ama hamburguer e doces mesmo sendo diabética.
+            Praia e locais com futebol fazem ela sentir paz e felicidade.
+            Laura é uma pessoa maravilhosa a qual todos merecem conhecer.
+            🐰🏃🏽🎓⚽🥇🧿📈🥼</p>
+        </div>
+      </div>
+    </>
+  )
+
+}
+
+export default SobreLaura
